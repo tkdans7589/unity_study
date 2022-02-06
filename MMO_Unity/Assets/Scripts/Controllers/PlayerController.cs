5ed1736b5d91d4f4e6391b42c0845568f637622d
tkdans7589 <32192086+tkdans7589@users.noreply.github.com>
2022-02-06 17:48:54 +0900

    chore : move files

--- MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
+++ MMO_Unity/Assets/Scripts/Controllers/PlayerController.cs
@@ -7,10 +7,15 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     [SerializeField]
     float _speed = 10.0f;
+
+    bool _moveToDest = false;
+    Vector3 _destPos;
     void Start()
     {
         Managers.Input.KeyAction -= OnKeyboard;
         Managers.Input.KeyAction += OnKeyboard;
+        Managers.Input.MouseAction -= OnMouseClicked;
+        Managers.Input.MouseAction += OnMouseClicked;
     }
 
     // GameObject (Player)
@@ -20,42 +25,70 @@ public class PlayerController : MonoBehaviour
     float _yAngle = 0.0f;
     void Update()
     {
-        
-            
+        if (_moveToDest)
+        {
+            Vector3 dir = _destPos - transform.position;
+            if (dir.magnitude < 0.0001f)
+            {
+                _moveToDest = false;
+            }
+            else
+            {
+                float moveDist = Mathf.Clamp(_speed * Time.deltaTime, 0, dir.magnitude);
+                transform.position += dir.normalized * moveDist;
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), 10 * Time.deltaTime);
+            }
+        }   
     }
     void OnKeyboard()
     {
        _yAngle += Time.deltaTime * 100.0f;
-        // transform.eulerAngles = new Vector3(0.0f, _yAngle, 0.0f);
-        // transform.Rotate(new Vector3(0.0f, Time.deltaTime * 100.0f, 0.0f));
-        // transform.rotation = Quaternion.Euler(new Vector3(0.0f, _yAngle, 0.0f));
 
         if (Input.GetKey(KeyCode.W))
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.forward), 0.2f);
-            // transform.Translate(Vector3.forward * Time.deltaTime * _speed);
             transform.position += Vector3.forward * Time.deltaTime * _speed;
         }
             
         if (Input.GetKey(KeyCode.S))
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.back), 0.2f);
-            // transform.Translate(Vector3.forward * Time.deltaTime * _speed);
             transform.position += Vector3.back * Time.deltaTime * _speed;
         }
             
         if (Input.GetKey(KeyCode.A))
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.left), 0.2f);
-            // transform.Translate(Vector3.forward * Time.deltaTime * _speed);
             transform.position += Vector3.left * Time.deltaTime * _speed;
         }
             
         if (Input.GetKey(KeyCode.D))
         {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(Vector3.right), 0.2f);
-            // transform.Translate(Vector3.forward * Time.deltaTime * _speed);
             transform.position += Vector3.right * Time.deltaTime * _speed;
-        } 
+        }
+
+        _moveToDest = false;
+    }
+
+    void OnMouseClicked(Define.MouseEvent evt)
+    {
+        if (evt != Define.MouseEvent.Click)
+            return;
+        
+        Debug.Log("OnMouseClicked");
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100.0f, LayerMask.GetMask("Wall")))
+        {
+            _destPos = hit.point;
+            _moveToDest = true;
+            // Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
+        }
     }
 }
